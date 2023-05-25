69c6cb7955f8005483d86951f20510fdee09e865
U-keun Song <sukeun319@gmail.com>
2023-05-25 22:04:35 +0900

    Scan할 Component 대상 추가

--- src/main/java/com/ukeun/webfluxcrud/WebfluxCrudApplication.java
+++ src/main/java/com/ukeun/webfluxcrud/WebfluxCrudApplication.java
@@ -3,9 +3,14 @@ package com.ukeun.webfluxcrud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
+@ComponentScan({"com.ukeun.webfluxcrud.router",
+        "com.ukeun.webfluxcrud.handler",
+        "com.ukeun.webfluxcrud.service",
+        "com.ukeun.webfluxcrud.repository"})
 @EntityScan("com.ukeun.webfluxcrud.entity")
 @EnableR2dbcRepositories("com.ukeun.webfluxcrud.repository")
 public class WebfluxCrudApplication {
